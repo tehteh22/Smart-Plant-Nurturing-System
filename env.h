2553539167ba374635f10480d2f5bec1b9e5b65e
Teh ZJ <tehteh22@hotmail.com>
2024-01-26 11:51:52 +0800

    Encryption for Device ID

---
+++ env.h
@@ -0,0 +1,11 @@
+#ifndef env.h
+#define env.h
+
+#define Dht11_module "00000000-0000-0000-0000-000000000000" // DHT11 replacing with your own device ID
+#define GL55_module "00000000-0000-0000-0000-000000000000" // GL55
+#define Soil_module "00000000-0000-0000-0000-000000000000" // Soil Moisture
+#define Water_module"00000000-0000-0000-0000-000000000000" // Water Level
+#define LED_module "00000000-0000-0000-0000-000000000000" // LED
+#define Relay_module "00000000-0000-0000-0000-000000000000" // Relay 
+
+#endif
